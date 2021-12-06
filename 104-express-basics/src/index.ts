b0373e718f4dc1e35fb07d3eeb701223630d6e2b
Sumit K <synergy2411@outlook.com>
2021-12-06 13:28:57 +0530

    get and delete endpoints

--- 104-express-basics/src/index.ts
+++ 104-express-basics/src/index.ts
@@ -1,14 +1,58 @@
-import express from 'express';
-import * as http from 'http';
+import express, { Request, Response } from 'express';
 
 const app = express()
 
-const server = http.createServer(app);
 
-app.use((req, res) => {
-    res.send("Hello from Express")
+let notes = [
+    { id : 101, title : "Shopping", body : "to buy the jeans"},
+    { id : 102, title : "Grocery", body : "to buy the pulses"},
+    { id : 103, title : "Planting", body : "to pot the plants"},
+]
+
+
+// POST - http://localhost:9001/notes
+
+// PATCH - http://localhost:9001/notes/101
+
+
+// DELETE - http://localhost:9001/notes/101
+app.delete("/notes/:noteId", (req, res) => {
+    const { noteId } = req.params;
+    const noteFound = notes.find(n => n.id === +noteId)
+    if(noteFound){
+        const duplicatedNotes = notes.filter(n => n.id !== +noteId)
+        notes = [...duplicatedNotes]
+        return res.status(200).send({...noteFound}) 
+    }else{
+        return res.status(404).send({message : "Not Found"})
+    }
 })
 
-server.listen(9090, "localhost", () =>{console.log("Express server running on PORT : 9090")})
+// GET Endpoints - http://localhost:9001/notes/101
+app.get("/notes/:noteId", (req, res) => {
+    const {noteId} = req.params;
+    const noteFound = notes.find(n => n.id === Number(noteId))
+    if(noteFound){
+        return res.status(200).send({...noteFound})
+    }else{
+        return res.status(404).send({error : "Note NOT found for ID - "+noteId})
+    }
+})
+
+// GET Endpoints - http://localhost:9001/notes
+app.get("/notes", (req, res) => {
+    console.log("URL : ", req.url);
+    res.json(notes)
+})
+
+
+// Endpoints - http://localhost:9001
+app.get("/", (req : Request, res : Response) => {
+    console.log("URL  :", req.url);
+    res.send("Hello from GET endpoint!")
+})
+
+app.listen(9001, () => {console.log("Express server started at PORT : 9001")})
+
 
 
