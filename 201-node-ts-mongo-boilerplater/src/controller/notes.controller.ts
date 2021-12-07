2ee3dea026bf288af66a32aee003355d4b953863
Sumit K <synergy2411@outlook.com>
2021-12-07 15:43:46 +0530

    crud with node and mongo

---
+++ 201-node-ts-mongo-boilerplater/src/controller/notes.controller.ts
@@ -0,0 +1,53 @@
+import { MongoClient } from "mongodb";
+
+const mongoURI =
+  "mongodb+srv://citius:0LXwebyFsFLhDmt2@cluster0.e9xsq.mongodb.net/citius-db?retryWrites=true&w=majority";
+const mongoLocalURI = "mongodb://localhost:27017";
+
+let _db: any;
+
+// MongoClient.connect(mongoURI)
+MongoClient.connect(mongoLocalURI)
+  .then((client) => {
+    console.log("Mongo Connected!");
+    _db = client.db("citius-db");
+  })
+  .catch((err) => {
+    console.log(err);
+    process.exit(1);
+  });
+
+export const deleteNote = () => {
+  _db
+    .collection("notes")
+    .deleteOne({ title: "shopping" }, (err: any, result: any) => {
+      if (err) console.log(err);
+      console.log(result);
+    });
+};
+
+export const createNote = () => {
+  _db
+    .collection("notes")
+    .insertOne(
+      { title: "planting", body: "to pot the plants" },
+      (err: any, result: any) => {
+        if (err) {
+          console.log(err);
+        }
+        console.log(result);
+      }
+    );
+};
+export const findNotes = () => {
+  return new Promise((resolve, reject) => {
+    _db.collection("notes")
+      .find()
+      .toArray((err: any, docs: any) => {
+          if(err) {
+              reject(err)
+          }
+        resolve(docs);
+      });
+  });
+};
