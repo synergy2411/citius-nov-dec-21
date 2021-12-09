a26b49354a3a19d0e2ecfdf1c64f64e24011b6a1
Sumit K <synergy2411@outlook.com>
2021-12-09 17:52:46 +0530

    error handling

--- 203-auth-app/src/index.ts
+++ 203-auth-app/src/index.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 
 const app = express()
@@ -9,7 +9,28 @@ const todos = [
     {label : 'shopping', status : true}
 ]
 
-app.get("/api/todos", (req, res) => {
+
+
+const m1 = (req : Request, res : Response, next : NextFunction) => {
+    console.log("Middleware 01")
+    next()
+}
+const m2 = (req : Request, res : Response, next : NextFunction) => {
+    console.log("Middleware 02")
+    // next("Some problem")
+    next()
+}
+const m3 = (req : Request, res : Response, next : NextFunction) => {
+    console.log("Middleware 03")
+    next()
+}
+// Error Middleware
+const errorHandler = (err : Error, req : Request, res : Response, next: NextFunction) =>{
+    
+    console.log("Something bad happened", err)
+}
+
+app.get("/api/todos", m1, m2, m3, errorHandler, (req : Request, res : Response) => {
     res.json(todos)
 })
 
