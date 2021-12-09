a96dc0d669d3dfa0effb3ede3a5bba364084abd0
Sumit K <synergy2411@outlook.com>
2021-12-09 13:12:11 +0530

    content negotiation

--- 203-auth-app/src/index.ts
+++ 203-auth-app/src/index.ts
@@ -16,10 +16,12 @@ const upload = multer({
     storage
 })
 
+// const upload = multer.memoryStorage()
 // const upload = multer({dest : 'tmp/'})
 
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
+app.use(express.static(__dirname + "/public"))
 
 app.get("/login", (req, res) => {
     res.sendFile(__dirname+"/public/index.html")
@@ -29,6 +31,33 @@ app.post("/login", upload.single('avatar') ,(req, res) => {
     console.log("Body : ", req.body);
     console.log("FILE : ", req.file)
     res.send({message : "SUCCESS"})
+})
+
+// Content Negotiation
+
+const iplWinners = [
+    { team : "SRH", year : 2014},
+    { team : "DD", year : 2013},
+    { team : "CSK", year : 2012},
+    { team : "CSK", year : 2009},
+]
+
+app.get("/negotiated", (req, res) => {
+    res.format({
+        'text/html' : () => { 
+            res.send(`<ul>
+            <li>${iplWinners[0].team}</li>
+            </ul>`)
+        },
+        'application/json' : () => res.json(iplWinners),
+        'text/plain' : () => {
+            let winners : string = '';
+            iplWinners.forEach(winner => {
+                winners += winner.team + " won in the year " + winner.year + "\n";
+            })
+            res.send(winners)
+        }
+    })
 })
 
 app.listen(9091, () =>console.log("Server started at PORT : 9091"))
